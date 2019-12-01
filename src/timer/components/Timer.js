dc4ebe2e2ae88db3874a31bed95e0eb2f7f26b1a
Kiririridev <bartek.kirej@gmail.com>
2019-12-01 23:42:22 +0100

    TimerButtonPad extractino from Timer.js

--- src/timer/components/Timer.js
+++ src/timer/components/Timer.js
@@ -1,39 +1,19 @@
 import * as React from 'react';
-import PomodoroTable from "./table/PomodoroTable";
-import ButtonPad from "./table/ButtonPad";
+import {TimerButtonPad} from "./table/TimerButtonPad";
 
 //todo fix bug
 //todo split to component and container
 //todo rewrite to functional component
 //todo plan redux
 
-const SAMPLE_DATA = {
-	pomodoroId: 0,
-	userId: "kiri",
-	pomodoroDate: "1994-06-23",
-	length: 20,
-	description: "testFetches",
-	tag: "TEST",
-};
+const formatTime = time => Math.round(time / 1000);
 
-//let pomodoros = getPomodoros();
+
+const renderTimer = (time) => <h3>Timer: {formatTime(time)}</h3>;
 
 export default class Timer extends React.Component {
 
 	timer;
-
-	constructor(props) {
-		super(props);
-		this.state = {
-			time: 0,
-			isOn: false,
-			start: 0,
-		};
-
-		this.startTimer = this.startTimer.bind(this);
-		this.stopTimer = this.stopTimer.bind(this);
-		this.resetTimer = this.resetTimer.bind(this);
-	}
 
 	startTimer() {
 		this.setState({
@@ -42,58 +22,46 @@ export default class Timer extends React.Component {
 			start: Date.now() - this.state.time,
 		});
 		this.timer = setInterval(() => this.setState({
-			time: Date.now() - this.state.start,
-		}), 1);
+			// time: Date.now() - this.state.start,
+			time: this.state.time + 1000,
+		}), 1000);
 	};
 
-
-	stopTimer() {
+	pauseTimer() {
+		clearInterval(this.timer);
 		this.setState({
 			isOn: false,
 		});
 	};
 
 	resetTimer() {
+		clearInterval(this.timer);
 		this.setState({
 			time: 0,
 			isOn: false,
 		});
 	};
 
+	constructor(props) {
+		super(props);
+		this.state = {
+			time: 0,
+			isOn: false,
+			start: 0,
+		};
+	}
+
 	render() {
-		//	console.log("TIMER" + JSON.stringify(pomodoros));
-
-		// let fetch = <button onClick={getPomodoros}>FETCH</button>;
-		// let postFetch = <button onClick={() => postPomodoro(SAMPLE_DATA)}>POST FETCH</button>;
-		let pomodoroTable = <PomodoroTable/>;
-		let buttonPad = <ButtonPad/>;
-
-		let start = (this.state.time === 0) ?
-			<button onClick={this.startTimer}>START</button> :
-			null;
-
-		let stop = (this.state.time == 0) ?
-			null :
-			<button onClick={this.stopTimer}>STOP</button>;
-
-		let resume = (this.state.time == 0 || this.state.isOn) ?
-			null :
-			<button onClick={this.startTimer}>RESUME</button>;
-
-		let reset = (this.state.time == 0 || this.state.isOn) ?
-			null :
-			<button onClick={this.resetTimer}>RESET</button>;
-
 		return (
 			<>
-				<h3>timer: {(this.state.time)}</h3>
-
-				{start}
-				{resume}
-				{stop}
-				{reset}
-				{pomodoroTable}
-				{buttonPad}
+				{renderTimer(this.state.time)}
+				<TimerButtonPad
+					startTimer={() => this.startTimer()}
+					pauseTimer={() => this.pauseTimer()}
+					resetTimer={() => this.resetTimer()}
+					isOn={this.state.isOn}
+					time={this.state.time}
+				/>
 			</>
 		);
 	}
